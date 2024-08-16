206fd025e5ec66230c75bce3cc9dfbc01ea5170f
eyzee00 <azizihamza1910@gmail.com>
2024-08-16 04:11:22 +0100

    Successfully Set Up The Frontend App, With A Basic UI Linking Between Registering, Logging in and Chatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,24 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import Chat from './pages/chat';
 import Login from './pages/login';
 import Register from './pages/register';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import NavBar from './components/navbar'
+
 
 function App() {
 
   return (
-    <><Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/" element={<Chat />} />
-      <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+    <>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Chat />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
     </>
   );
 }
