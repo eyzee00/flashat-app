d07267d427a507434735132e5cf958bc7868bc8e
eyzee00 <azizihamza1910@gmail.com>
2024-08-16 05:10:32 +0100

    Added Proper Forms For Registering And Logging In, The UI Is Still Basic

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,5 +1,28 @@
+import { Form, Row, Col, Stack, Alert, Button } from "react-bootstrap";
+
 const Login = () => {
-    return ( <>Login</> );
+    return (
+    <>
+    <Form>
+        <Row style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%"
+        }}>
+            <Col xs={6}>
+                <Stack gap={3}>
+                    <h2>Log In</h2>
+
+                    <Form.Control type="email" placeholder="Email"/>
+                    <Form.Control type="password" placeholder="Password"/>
+                    <Button variant="primary" type="submit">Login</Button>
+                    <Alert variant="info">Have not created an account yet? <Alert.Link href="/login">Register Now!</Alert.Link></Alert>
+                </Stack>
+            </Col>
+        </Row>
+    </Form>
+    </>
+    );
 }
  
 export default Login;
--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,5 +1,29 @@
+import { Form, Row, Col, Stack, Alert, Button } from "react-bootstrap";
+
 const Register = () => {
-    return ( <>Register</> );
+    return (
+    <>
+    <Form>
+        <Row style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%"
+        }}>
+            <Col xs={6}>
+                <Stack gap={3}>
+                    <h2>Register Now And Start Chatting!</h2>
+
+                    <Form.Control type="text" placeholder="Name"/>
+                    <Form.Control type="email" placeholder="Email"/>
+                    <Form.Control type="password" placeholder="Password"/>
+                    <Button variant="primary" type="submit">Register</Button>
+                    <Alert variant="info">Already have an account? <Alert.Link href="/login">Login Now!</Alert.Link></Alert>
+                </Stack>
+            </Col>
+        </Row>
+    </Form>
+    </>
+    );
 }
  
 export default Register;
