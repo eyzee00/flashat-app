a08bf21e80075157fd504c56da8ef05323e039e1
eyzee00 <azizihamza1910@gmail.com>
2024-08-22 08:10:36 +0100

    Added More Advanced Styling, And Created A component That Handles Listing Chats For A Specific User

--- client/src/pages/chat.jsx
+++ client/src/pages/chat.jsx
@@ -1,10 +1,30 @@
 import { useContext } from "react";
+import { Container, Stack } from "react-bootstrap";
 import { ChatContext } from "../context/chatContext";
+import { AuthContext } from "../context/authContext";
+import  UserChat from "../components/chat/userChat";
 
 const Chat = () => {
+    const user = useContext(AuthContext);
     const { userChats, isUserChatsLoading, userChatsError } = useContext(ChatContext);
-    console.log("UserChats: ", userChats);
-    return ( <>Chat</> );
+    return (
+    <>
+        <Container>
+            {userChats?.length < 1 ? null : (
+                <Stack direction="horizontal" className="align-items-start" gap={6}>
+                    <Stack direction="vertical" className="messages-box flex-grow-0 pe-3" gap={3}>
+                        {isUserChatsLoading && <p>Loading chats...</p>}
+                        {userChats?.map(( chat, index ) => (
+                            <div key={index}>
+                                <UserChat chat={chat} user={user}/>
+                            </div>
+                        ))}
+                    </Stack>
+                    <p>ChatBox</p>
+                </Stack>
+            )}
+        </Container>
+    </> );
 }
  
 export default Chat;
