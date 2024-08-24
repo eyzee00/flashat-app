901b6a5074686b36a2021de76b3b15049cb8e73f
eyzee00 <azizihamza1910@gmail.com>
2024-08-24 07:32:53 +0100

    Successfully Added Real Time Communication Thanks To Socketio, Also Users Can See When Others Are Online Or Offline

---
+++ socket/index.js
@@ -0,0 +1,32 @@
+const { Server } = require('socket.io');
+
+const io = new Server({ cors: "http://localhost:5173" });
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+    console.log("New client connected", socket.id);
+
+    socket.on("addNewUser", (userId) => {
+ 
+        if (!onlineUsers.some((user) => user.userId === userId) && userId) { 
+            onlineUsers.push({ userId, socketId: socket.id });
+        }
+        io.emit("getOnlineUsers", onlineUsers);
+    });
+
+    socket.on("sendMessage", (message) => {
+        const recipient = onlineUsers.find((user) => user.userId === message.recipientId);
+        if (recipient) {
+            io.to(recipient.socketId).emit("getMessage", message);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+        io.emit("getOnlineUsers", onlineUsers);
+    });
+});
+
+
+io.listen(3080);
